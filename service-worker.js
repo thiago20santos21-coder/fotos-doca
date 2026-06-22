39dbb889bc24b0a3c0a8ee252a5c232e5170f3ca
Tiago <tiago21lima22@gmail.com>
2026-06-22 19:21:27 -0300

    fix: bump SW cache to v2 para limpar cache antigo

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'fotos-doca-v1';
+const CACHE = 'fotos-doca-v2';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
